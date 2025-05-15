8dc0efcfc57db5ca1753c8db84899099c5085a2b
Juhana Kommeri <juhana.kommeri@gmail.com>
2025-05-15 15:36:11 +0300

    First pre-release

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ChoicelySDK",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ChoicelySDK",
+            targets: ["ChoicelySDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "ChoicelySDK",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.1/ChoicelySDKCore.xcframework.zip",
+            checksum: "1a1adb6711ce7b0887ca370d8fd7f291d5f0ba9066879d7cf9244feb50eeb61e"
+        ),
+    ],
+    swiftLanguageModes: [.v5]
+)
