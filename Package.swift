5b3503031aba6670a23d619ba2bc82853274d301
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-05-16 17:13:48 +0300

    0.0.7

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChoicelySDKCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.6/ChoicelySDKCore.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.7/ChoicelySDKCore.xcframework.zip",
             checksum: "1a1adb6711ce7b0887ca370d8fd7f291d5f0ba9066879d7cf9244feb50eeb61e"
         ),
         .binaryTarget(
             name: "ChoicelySDKShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.6/ChoicelySDKShop.xcframework.zip",
-            checksum: "4cd635892147fc229909d76bd710aef32263a1d8ba7b18abc86cd2c475693fec"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.7/ChoicelySDKShop.xcframework.zip",
+            checksum: "d0ad28ea6fbd45923d69641b1037720853e01b109314eb3199df18c3accd5ac1"
         )
     ]
 )
