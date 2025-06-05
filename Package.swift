07098a4b0fc0edc0f42063ad06b14cb335c109c7
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-06-05 16:52:06 +0300

    Revert to 0.0.9

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChoicelySDKCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.14/ChoicelySDKCore.xcframework.zip",
-            checksum: "sha256:e566892daf29beb4e24b778270720d22fd47f52531299700d04f5f43d6d8481a"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.9/ChoicelySDKCore.xcframework.zip",
+            checksum: "2d0ae6b2e3019c65f35920f8613b5ad5e6d23a34e1e5fb6ee82ae1eb3dc9f896"
         )
     ]
 )
