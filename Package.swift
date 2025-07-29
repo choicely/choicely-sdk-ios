69c68769117485b20340773f10abad38fb6d8c1a
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-07-29 14:48:09 +0300

    GoogleMaps import as xcframework

--- Package.swift
+++ Package.swift
@@ -12,21 +12,21 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "git@github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10")),
-        .package(url: "git@github.com:googlemaps/ios-maps-sdk.git", .upToNextMajor(from: "8.4.0")),
+        .package(url: "git@github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10"))
     ],
     targets: [
         .target(
             name: "ChoicelySDK",
             dependencies: [
-                .product(name: "GoogleMaps", package: "ios-maps-sdk"),
                 "FlexLayout",
-                .target(name: "ChoicelySDKCore")
+                .target(name: "ChoicelySDKCore"),
+                .target(name: "GoogleMaps")
             ]
         ),
+        .binaryTarget(name: "GoogleMaps", path: "Artifacts/GoogleMaps.xcframework"),
         .binaryTarget(
             name: "ChoicelySDKCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.13/ChoicelySDKCore.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.14/ChoicelySDKCore.xcframework.zip",
             checksum: "478a7442250ee6713f86315b49a0ce24a61755db6944b5f62219407891094167"
         )
     ]
