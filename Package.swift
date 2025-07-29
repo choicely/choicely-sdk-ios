78aeab328ab46307344fbb3104ebfbe129403954
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-07-29 13:28:00 +0300

    Flex dependency added

--- Package.swift
+++ Package.swift
@@ -4,18 +4,33 @@ import PackageDescription
 
 let package = Package(
     name: "ChoicelySDK",
-    defaultLocalization: "en",
     platforms: [.iOS(.v14)],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ChoicelySDKCore",
-            targets: ["ChoicelySDKCore"]
-        )
+            name: "ChoicelySDK",
+            targets: ["ChoicelySDK"]),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10")),
+        //.package(url: "git@github.com:googlemaps/ios-maps-sdk.git", .upToNextMajor(from: "8.4.0")),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ChoicelySDK",
+            dependencies: [
+                //.product(name: "GoogleMaps", package: "ios-maps-sdk"),
+                //.product(name: "GoogleMapsBase", package: "ios-maps-sdk"),
+                //.product(name: "GoogleMapsCore", package: "ios-maps-sdk"),
+                "FlexLayout",
+                .target(name: "ChoicelySDKCore")
+            ]
+        ),
         .binaryTarget(
             name: "ChoicelySDKCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.11/ChoicelySDKCore.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.12/ChoicelySDKCore.xcframework.zip",
             checksum: "478a7442250ee6713f86315b49a0ce24a61755db6944b5f62219407891094167"
         )
     ]
---
+++ Sources/ChoicelySDK/ChoicelySDK.swift
@@ -0,0 +1,10 @@
+//
+//  TestClass.swift
+//  ChoicelySDK
+//
+//  Created by Oleksandr Kryvodub on 29.7.2025.
+//
+
+import Foundation
+
+class TestClass {}
