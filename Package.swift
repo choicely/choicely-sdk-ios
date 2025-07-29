61b5a9efaaa19cc9a65887f2a2c79fba847210d4
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-07-29 20:41:22 +0300

    0.0.18

--- Package.swift
+++ Package.swift
@@ -12,24 +12,20 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "git@github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10")),
-        .package(url: "git@github.com:googlemaps/ios-maps-sdk.git", .upToNextMajor(from: "8.4.0")),
+        .package(url: "git@github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10"))
     ],
     targets: [
         .target(
             name: "ChoicelySDK",
             dependencies: [
-                .product(name: "GoogleMaps", package: "ios-maps-sdk"),
-                .product(name: "GoogleMapsBase", package: "ios-maps-sdk"),
-                .product(name: "GoogleMapsCore", package: "ios-maps-sdk"),
                 "FlexLayout",
                 .target(name: "ChoicelySDKCore")
             ]
         ),
         .binaryTarget(
             name: "ChoicelySDKCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.17/ChoicelySDKCore.xcframework.zip",
-            checksum: "df0a3c6625bb97fdd2b83e279f22285b8e30b3208836e398d4dc66ab5c4b0ba6"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.18/ChoicelySDKCore.xcframework.zip",
+            checksum: "5ff039eb5c53fa251b78b465b694cab25725e6a7ed9b22be3edfd59b4d06bbb3"
         )
     ]
 )
