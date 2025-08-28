55d72f565b58eeb1871bbb1ecae5f878a19398d9
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-08-28 16:04:09 +0300

    0.0.31

--- Package.swift
+++ Package.swift
@@ -40,27 +40,27 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChoicelyCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.29/ChoicelyCore.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.31/ChoicelyCore.xcframework.zip",
             checksum: "93ab85c8cf50d3220c4936a410f8aa0dc2759c64798c463971a4c612c85d8c1c"
         ),
         .binaryTarget(
             name: "ChoicelyMap",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.29/ChoicelyMap.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.31/ChoicelyMap.xcframework.zip",
             checksum: "3353b7c87ca5765adc285020b8d0094b8f3b57ea4410d7096e7bf0beedf1f011"
         ),
         .binaryTarget(
             name: "ChoicelyFirebase",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.29/ChoicelyFirebase.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.31/ChoicelyFirebase.xcframework.zip",
             checksum: "78af3bd7c742b1a0127b9330697903708699a6b725319ffc810610842d9b00b7"
         ),
         .binaryTarget(
             name: "ChoicelyShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.29/ChoicelyShop.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.31/ChoicelyShop.xcframework.zip",
             checksum: "e4ad5fe0d1ca11c6782ebafba64e07b3c134b72e856d471d24974e26bb744397"
         ),
         .binaryTarget(
             name: "ChoicelyAd",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.29/ChoicelyAd.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.31/ChoicelyAd.xcframework.zip",
             checksum: "fd44076cecc88273a7b43b7f5b5275d4a1d765ce88b7ddee629774d8e3b5333c"
         )
     ]
---
+++ Sources/ChoicelyCoreWrapper/ChoicelyCoreTest.swift
@@ -0,0 +1,10 @@
+//
+//  ChoicelyCoreTest.swift
+//  choicely-sdk-ios
+//
+//  Created by Oleksandr Kryvodub on 28.8.2025.
+//
+
+import Foundation
+
+public class ChoicelyCoreTest {}
