e556dfc0a78c6b7f4360014ea4bb9e5557f7aaf8
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-09-19 17:04:55 +0300

    0.0.72

--- Package.swift
+++ Package.swift
@@ -38,23 +38,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChoicelyCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.71/ChoicelyCore.xcframework.zip",
-            checksum: "752492231977357934e3f709d831dab232eb6ac1a2788309e3b97b06491e4102"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.72/ChoicelyCore.xcframework.zip",
+            checksum: "93de5e11b3078e8671f85ee69a942d1077f7bda75f77f774db1b7e7b6f2f525c"
         ),
         .binaryTarget(
             name: "ChoicelyMap",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.71/ChoicelyMap.xcframework.zip",
-            checksum: "ee341d3b23941fe35e6d058c8bf6cac976bacc7585908841587a257c283397b7"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.72/ChoicelyMap.xcframework.zip",
+            checksum: "9a3badd7584274a64c5e9c00118cc8710fd9f070323346512f2fa715726e3b8c"
         ),
         .binaryTarget(
             name: "ChoicelyFirebase",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.71/ChoicelyFirebase.xcframework.zip",
-            checksum: "1cdf40210856fcc256287bb15211d3b61a40b70b983e7bc19f21ae810aae41c0"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.72/ChoicelyFirebase.xcframework.zip",
+            checksum: "04c2b4c52874db9287298a5087d8cf2b27920afea0bf2b4511dcf23853046c61"
         ),
         .binaryTarget(
             name: "ChoicelyShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.71/ChoicelyShop.xcframework.zip",
-            checksum: "1ba2ecfb1d4bea25751476232f8861e9a87c1cfc48f8ae35ba5f8096ca96db0d"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.0.72/ChoicelyShop.xcframework.zip",
+            checksum: "ee506f604254b24b9a0af9aaadfbe61c8bdfa6aa52e56b77c14f46bb9647ac75"
         )
     ]
 )
