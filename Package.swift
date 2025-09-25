ca5261ea30ac46c6a5b5db465a5cf1d0383bfca8
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-09-25 17:17:39 +0300

    0.9.16

--- Package.swift
+++ Package.swift
@@ -38,23 +38,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChoicelyCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.15/ChoicelyCore.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.16/ChoicelyCore.xcframework.zip",
             checksum: "455ec7aaa51d3fab7e6a2fae6292fe881a54e6e3b12d5d7d987f988de1d4c6f7"
         ),
         .binaryTarget(
             name: "ChoicelyMap",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.15/ChoicelyMap.xcframework.zip",
-            checksum: "d44c56aca2ce9d15ba44d95d64d7924835688a55a9860a8f22dbfc8660e62fd6"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.16/ChoicelyMap.xcframework.zip",
+            checksum: "0878de9f1685e497d521d0cf5c9a7a02ea93286ebb95141feb2160a0618dc1b1"
         ),
         .binaryTarget(
             name: "ChoicelyFirebase",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.15/ChoicelyFirebase.xcframework.zip",
-            checksum: "ca354cbb5dcbd3daeba17fa8e589e737af63650e813c5a890db8f77eb7d0bb8e"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.16/ChoicelyFirebase.xcframework.zip",
+            checksum: "1b1e18c3057c6e1b73fe236788ecc6c3abdf94db1df5fee94b289286d54b85b8"
         ),
         .binaryTarget(
             name: "ChoicelyShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.15/ChoicelyShop.xcframework.zip",
-            checksum: "d302e2b0d2b883d08042561c3dcb66daa3243a3e055446128924cbca77bd0645"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/0.9.16/ChoicelyShop.xcframework.zip",
+            checksum: "3da30af21ca1950e4f1a7fc01a0aca39dcb8c42c6fd0c9a6648c3229c1a672a1"
         )
     ]
 )
