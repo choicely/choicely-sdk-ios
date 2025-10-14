8b9e0b0ef867de780199606b2bd8c683b101666b
Oleksandr Kryvodub <o.kryvodub@gmail.com>
2025-10-14 10:36:14 +0300

    1.0.2

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "git@github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10")),
-        .package(url: "git@github.com:Cocoanetics/DTCoreText.git", .upToNextMajor(from: "1.6.28"))
+        .package(url: "https://github.com:layoutBox/FlexLayout.git", .upToNextMajor(from: "2.0.10")),
+        .package(url: "https://github.com:Cocoanetics/DTCoreText.git", .upToNextMajor(from: "1.6.28"))
     ],
     targets: [
         .target(
@@ -38,22 +38,22 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChoicelyCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.1/ChoicelyCore.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.2/ChoicelyCore.xcframework.zip",
             checksum: "20321dbe23f19a57c77fc19df96e21a6f38f34d0145de1a63ae592130291a954"
         ),
         .binaryTarget(
             name: "ChoicelyMap",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.1/ChoicelyMap.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.2/ChoicelyMap.xcframework.zip",
             checksum: "c02ef3a5d0a0158fab529f0d6b39214de7e7a9b2be39014e2435a7a8aa1a8bc0"
         ),
         .binaryTarget(
             name: "ChoicelyFirebase",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.1/ChoicelyFirebase.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.2/ChoicelyFirebase.xcframework.zip",
             checksum: "58555bb16cc59fb6857c1183c8d9755e5c6e4ed8fb1127ef9afb1614c6bfc7fc"
         ),
         .binaryTarget(
             name: "ChoicelyShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.1/ChoicelyShop.xcframework.zip",
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.0.2/ChoicelyShop.xcframework.zip",
             checksum: "f0800ec127efc349b8c39d2a7388dd42bd0c537e049185de43d1486db5877791"
         )
     ]
