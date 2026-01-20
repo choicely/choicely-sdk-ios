0ec2cd3b5086382d69cbd88c88dc9c8039ffa864
Oleksandr Kryvodub <o.kryvodub@gmail.com>
2026-01-20 14:51:56 +0200

    1.2.5

--- Package.swift
+++ Package.swift
@@ -38,23 +38,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChoicelyCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.4/ChoicelyCore.xcframework.zip",
-            checksum: "81c47aac70b0996836f8d1b1466881a3b07ddeb78227db417f1c9f2a61c987bf"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.5/ChoicelyCore.xcframework.zip",
+            checksum: "6deba3754a7600a9ae0bb4ed7bce9d294c6c22fb4a36dc33a6ad577add6da09b"
         ),
         .binaryTarget(
             name: "ChoicelyMap",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.4/ChoicelyMap.xcframework.zip",
-            checksum: "01bd66d1784660a79e0d5a565b77c698d56178a0ef9e3d1a213b0c8481d07630"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.5/ChoicelyMap.xcframework.zip",
+            checksum: "c921228b09b18fa5d044a9cac0956225c374467d34a9ab0975a0b3822bc28e47"
         ),
         .binaryTarget(
             name: "ChoicelyFirebase",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.4/ChoicelyFirebase.xcframework.zip",
-            checksum: "59fffd7910e583ade1644dd772db65661ed328aa84cd086c83e5687e7ef1f39e"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.5/ChoicelyFirebase.xcframework.zip",
+            checksum: "846e8786856fc1692f92f876cb4d05bc5db60a554f81864c2920e909847c485a"
         ),
         .binaryTarget(
             name: "ChoicelyShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.4/ChoicelyShop.xcframework.zip",
-            checksum: "e300064dc873555c72082c8e5ab2713a321781a657bc5e11e8955854729d77f1"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.2.5/ChoicelyShop.xcframework.zip",
+            checksum: "db83aaac86f1cb7e4ef073a2984b39d1b8baff22c06d4c74e291be6601c78bac"
         )
     ]
 )
