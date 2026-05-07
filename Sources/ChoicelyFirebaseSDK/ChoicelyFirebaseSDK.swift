3af15b1b6cfa94dbcdb5cf0796b339e8038b83d6
Juhana Kommeri <juhana@choicely.com>
2026-05-07 11:20:19 +0300

    Add missing source file

---
+++ Sources/ChoicelyFirebaseSDK/ChoicelyFirebaseSDK.swift
@@ -0,0 +1,6 @@
+//
+//  ChoicelyFirebaseSDK.swift
+//  Anchors firebase-ios-sdk + FirebaseUI-iOS package products onto the
+//  ChoicelyFirebase binary target so consumer apps can resolve the static
+//  symbols (FIRAnalytics, FBSDK*) at link time.
+//
