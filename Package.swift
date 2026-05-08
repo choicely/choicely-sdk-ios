aeeb8901e40e4f75b10e330f5d666978bd38f851
Juhana Kommeri <juhana@choicely.com>
2026-05-08 11:43:00 +0300

    Release version 1.3.6

--- Package.swift
+++ Package.swift
@@ -32,43 +32,43 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ChoicelyCore",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/ChoicelyCore.xcframework.zip",
-            checksum: "1f35cf082f62682e69e3b1d61bb6f9ea9bec286123f34472eaf95287e31ec785"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/ChoicelyCore.xcframework.zip",
+            checksum: "951c09be92437fcd1cac07bb7668f066a33afbf3fe56a22abb8076657ea7a217"
         ),
         .binaryTarget(
             name: "ChoicelyMap",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/ChoicelyMap.xcframework.zip",
-            checksum: "69f06d972986ace50f6c223c89c00df91f7266c29e63c369bcfc7c68f6ef1908"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/ChoicelyMap.xcframework.zip",
+            checksum: "6b3f1b18d191609af565d078cf94384e9feaa42c9cfdf10f5e1ab244c13c0b2b"
         ),
         .binaryTarget(
             name: "ChoicelyFirebase",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/ChoicelyFirebase.xcframework.zip",
-            checksum: "a3cdc6b3d8b64a0adc56372c7f3da463f5a096ef82625abdaf05cf6b6be63961"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/ChoicelyFirebase.xcframework.zip",
+            checksum: "4edd6c62fe94233baa7c47660705cb3a65b49f994a8a874f832ca286b5b24b8c"
         ),
         .binaryTarget(
             name: "ChoicelyShop",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/ChoicelyShop.xcframework.zip",
-            checksum: "a41f8a56c2dbf90f69db3a49171af87fe1ea8ec92b788a5129d124abb27b6512"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/ChoicelyShop.xcframework.zip",
+            checksum: "8a4147d1a2198183e8859b3f13492ea3fdcb3956302df4ccd963a450c6e22513"
         ),
         .binaryTarget(
             name: "ChoicelyReactNativeEngine",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/ChoicelyReactNativeEngine.xcframework.zip",
-            checksum: "25c750624c649da4ec6a93e2877025756398d1f77f5e5c16131f71b7b0de6445"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/ChoicelyReactNativeEngine.xcframework.zip",
+            checksum: "69af7d6d1df46b8fd74b57c51bbc12eb545efa4abe04b5e113c0a7684da92f8e"
         ),
         .binaryTarget(
             name: "hermesvm",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/hermesvm.xcframework.zip",
-            checksum: "30126799d6c30536c4a662719a1ac575fa7618e71fa1d3a165b7868d2160ca78"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/hermesvm.xcframework.zip",
+            checksum: "e5cd5f0201d4ce742a7ecbebe579b7beab7da7714a040672f8ac7afe18f0e71d"
         ),
         .binaryTarget(
             name: "rnllama",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/rnllama.xcframework.zip",
-            checksum: "40f31cbf49f7902590137993a57de32e8fb4069b0a9e43580df02d87acfc33f2"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/rnllama.xcframework.zip",
+            checksum: "9cfa16a21e91953b145f0b20319e6e5ef9e45781d8be96e89904fffdd3abe6f6"
         ),
         .binaryTarget(
             name: "ChoicelyReactNative",
-            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.5/ChoicelyReactNative.xcframework.zip",
-            checksum: "72b75ce659951aec6ef24ef8621dd6b78a67f56ffa3fe7c330308a9571fcbffb"
+            url: "https://github.com/choicely/choicely-sdk-ios/releases/download/1.3.6/ChoicelyReactNative.xcframework.zip",
+            checksum: "fde3e1b96ba7c662ac650e87d35b255b91efa453c7185542e7327a37f315a205"
         )
     ]
 )
